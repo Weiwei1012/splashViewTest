7bcd58f30b0aa7bbd3b1f9f70d37fd83a579acb9
Weiwei <tyler22501789@gmail.com>
2021-01-04 00:20:09 +0800

    Initial Commit

---
+++ splashViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splashViewTest
+//
+//  Created by 楊智崴 on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
